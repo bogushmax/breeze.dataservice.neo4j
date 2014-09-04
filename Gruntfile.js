ddebcbf672c2b74a06bb85a1a49c844c60c7d246
Bogush Max <bogushmax@gmail.com>
2014-09-04 22:33:17 +0400

    fixed jasmine tests;

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,23 +3,33 @@ module.exports = function( grunt ) {
     'use strict';
 
     grunt.initConfig({
-            pkg: grunt.file.readJSON('package.json'),
-            jasmine: {
-                dev: {
-                    src: 'src/breeze.dataservice.neo4j.js',
-                    options: {
-                        specs: 'test/specs/dev/**/*spec.js',
-                        keepRunner: true,
-                        template: require('grunt-template-jasmine-requirejs'),
-                        templateOptions: {
-                            requireConfig: requireConfig
+        pkg: grunt.file.readJSON('package.json'),
+        jasmine: {
+            dev: {
+                src: 'src/breeze.dataservice.neo4j.js',
+                options: {
+                    specs: 'test/specs/dev/**/*spec.js',
+                    keepRunner: true,
+                    template: require('grunt-template-jasmine-requirejs'),
+                    templateOptions: {
+                        requireConfig: {
+                            paths: {
+                                'breeze': 'lib/breezejs/breeze.debug',
+                                'jquery': 'lib/jquery/dist/jquery',
+                                'q': 'lib/q/q'
+                            },
+                            shim: {
+                                breeze: {
+                                    deps: ['jquery', 'q']
+                                }
+                            }
                         },
                         vendor: ['node_modules/jasmine-ajax/lib/mock-ajax.js']
                     }
                 }
             }
         }
-    );
+    });
 
 // Loading plugin(s)
     grunt.loadNpmTasks('grunt-contrib-jasmine');
